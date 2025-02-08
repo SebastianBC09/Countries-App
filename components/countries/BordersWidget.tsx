bf7b26f69f380e96f7f4126299cac86c0b72abd2
Sébastien Ballen Castaneda <sebastian.ballenc@gmail.com>
2025-02-08 10:09:36 -0500

    fix: remove use client for components that doest need it

--- components/countries/BordersWidget.tsx
+++ components/countries/BordersWidget.tsx
@@ -1,7 +1,5 @@
-'use client';
-
-import { motion } from 'framer-motion';
 import Link from 'next/link';
+import { motion } from 'framer-motion';
 import type { CountryDetail } from '@/lib/types/country';
 
 interface BordersWidgetProps {
