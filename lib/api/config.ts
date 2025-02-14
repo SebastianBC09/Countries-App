799abd81b8afb0a55ee7c968ed6bbcd6548977e2
Sébastien Ballen Castaneda <sebastian.ballenc@gmail.com>
2025-02-13 20:27:21 -0500

    fix

--- lib/api/config.ts
+++ lib/api/config.ts
@@ -1,5 +1,5 @@
 export const API_CONFIG = {
-  baseURL: process.env.NEXT_PUBLIC_API_BASE_URL,
+  baseURL: process.env.NEXT_PUBLIC_API_BASE_URL || 'http://countries-be.us-east-2.elasticbeanstalk.com/api',
   timeout: Number(process.env.NEXT_PUBLIC_API_TIMEOUT) || 30000,
   headers: {
     'Content-Type': 'application/json',
